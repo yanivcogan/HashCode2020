2007f130f3a49fbd5b4b22fc8d735ba776d71f59
shaul.rosner shaul.rosner <shaul.rosner@gmail.com>
2020-02-20 21:25:21 +0200

    fixed duplicate libraries

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from classes import *
 from solver import greedy
 
 ALL_FILES = [
-    "a_example.txt",
-    "b_read_on.txt",
-    "c_incunabula.txt",
-    "d_tough_choices.txt",
+    #"a_example.txt",
+    #"b_read_on.txt",
+    #"c_incunabula.txt",
+    #"d_tough_choices.txt",
     "e_so_many_books.txt",
     "f_libraries_of_the_world.txt",
 ]
--- solver.py
+++ solver.py
@@ -6,11 +6,15 @@ from book_choice import BookChooser
 def greedy(libraries: List[Library], book_scores: List[int], days_left: int) -> Solution:
     selector = BookChooser(days_left, book_scores)
     print(f"days left: {days_left}")
+    selected_indices = set()
     while days_left >= 0:
         selected_library = pick_library(libraries, book_scores, days_left, selector.scanned_books)
         if selected_library is None:
             print(f"ran out of libraries to pick")
             break
+        if selected_library.index in selected_indices:
+            break
+        selected_indices.add(selected_library.index)
         days_left -= selected_library.signup_time
         selector.add_library(selected_library, days_left)
         print(f"picked {selected_library.index}. days left: {days_left}")
