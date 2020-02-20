ee43a3b95cc1836735aadbfacb5c3c3cea478357
itamar <itamar@feezback.com>
2020-02-20 21:34:16 +0200

    add optimization, but not a good enough one

--- solver.py
+++ solver.py
@@ -1,14 +1,25 @@
+from typing import Dict
+
 from classes import *
 from library_picker import pick_library
 from book_choice import BookChooser
 
 
 def greedy(libraries: List[Library], book_scores: List[int], days_left: int) -> Solution:
+    # optimization:
+    # instead of working with a hist set that is constantly being intersected,
+    # each library remembers set of remaining book ids
+    # when picking a library, only update remaining book ids for other libraries that share those books
+    libraries_by_book: Dict[int, List[Library]] = {i: [] for i in range(len(book_scores))}
+    for library in libraries:
+        for book_id in library.book_ids:
+            libraries_by_book[book_id].append(library)
+
     selector = BookChooser(days_left, book_scores)
     print(f"days left: {days_left}")
     selected_indices = set()
     while days_left >= 0:
-        selected_library = pick_library(libraries, book_scores, days_left, selector.scanned_books)
+        selected_library = pick_library(libraries, book_scores, days_left)
         if selected_library is None:
             print(f"ran out of libraries to pick")
             break
@@ -17,6 +28,9 @@ def greedy(libraries: List[Library], book_scores: List[int], days_left: int) -> 
         selected_indices.add(selected_library.index)
         days_left -= selected_library.signup_time
         selector.add_library(selected_library, days_left)
+        for book_id in selected_library.book_ids_remaining.copy():
+            for library in libraries_by_book[book_id]:
+                library.book_ids_remaining.remove(book_id)
         print(f"picked {selected_library.index}. days left: {days_left}")
     return selector.get_scans()
 
