5dd2cf9d3eafbb1f44c1324643301470d80d5c7d
itamar <itamar@feezback.com>
2020-02-20 20:44:32 +0200

    also prettify constructor

--- book_choice.py
+++ book_choice.py
@@ -4,7 +4,7 @@ from classes import LibraryScan, Library
 
 
 class BookChooser:
-    def __init__(self, days: int, scores: List[int]):
+    def __init__(self, scores: List[int]):
         self.scores: List[int] = scores
         self.scanned_books: Set[int] = set()
         self.library_scans: List[LibraryScan] = []
