4a4faf991339124e9fe10899abbb3debcc8966e5
itamar <itamar@feezback.com>
2020-02-20 19:46:52 +0200

    make main greaterer

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import math
 
+ALL_FILES = [
+    "./a_example.in",
+    "./b_small.in",
+    "./c_medium.in",
+    "./d_quite_big.in",
+    "./e_also_big.in",
+]
+OPENED_FILE = ALL_FILES[0]
+
 
 def main():
     print("---START---")
+    data = OPENED_FILE
+    with open(data) as file:
+        lines = file.readlines()
+    first_line_parts = lines[0].split(' ')
 
 
 if __name__ == '__main__':
