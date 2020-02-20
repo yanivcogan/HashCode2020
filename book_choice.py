306e1fde0129789018b411bf05991fc36b5e79ee
itamar <itamar@feezback.com>
2020-02-20 20:37:06 +0200

    reformat shaul's code

--- book_choice.py
+++ book_choice.py
@@ -4,17 +4,18 @@ from classes import LibraryScan, Library
 
 
 class BookChooser:
-    def __init__(self, days:int, scores:List[int]):
-        self.scores:List[int] = scores
-        self.days:int = days
-        self.scanned_books:set = set()
-        self.library_scans:List[LibraryScan] = []
+    def __init__(self, days: int, scores: List[int]):
+        self.scores: List[int] = scores
+        self.days: int = days
+        self.scanned_books: set = set()
+        self.library_scans: List[LibraryScan] = []
 
-    def add_library(self, library:Library, days_to_scan:int) -> None:
-        sorted_books:List[int] = sorted(library.book_ids, key=lambda book_id:self.scores[book_id], reverse=True)
-        added_books:int = 0
-        chosen_books:List[int] = []
-        needed_books:int = days_to_scan*library.ship_rate
+    def add_library(self, library: Library, days_to_scan: int) -> None:
+        sorted_books: List[int] = sorted(library.book_ids, key=lambda book_id: self.scores[book_id],
+                                         reverse=True)
+        added_books: int = 0
+        chosen_books: List[int] = []
+        needed_books: int = days_to_scan * library.ship_rate
         for book_id in sorted_books:
             if book_id in self.scanned_books:
                 continue
@@ -25,8 +26,9 @@ class BookChooser:
                 if added_books >= needed_books:
                     break
         if len(chosen_books) < needed_books:
-            print (f"WARNING: Library {library.index} has {days_to_scan} days but scanned {len(chosen_books)} books")
+            print(
+                f"WARNING: Library {library.index} has {days_to_scan} days but scanned {len(chosen_books)} books")
         self.library_scans.append(LibraryScan(library, chosen_books))
 
-    def get_scans(self)->List[LibraryScan]:
+    def get_scans(self) -> List[LibraryScan]:
         return self.library_scans
