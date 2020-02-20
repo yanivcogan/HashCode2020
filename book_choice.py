4702209141e6510892c411ecbca8ddcd88574f79
shaul.rosner shaul.rosner <shaul.rosner@gmail.com>
2020-02-20 20:17:02 +0200

    added book choice function

--- book_choice.py
+++ book_choice.py
@@ -8,5 +8,16 @@ class BookChooser:
     def add_library(self, library, days_to_scan):
         sorted_books = sorted(list(library.book_ids), key=lambda book_id:self.scores[book_id], reverse=True)
         added_books = 0
-        for book in sorted_books:
-            
+        chosen_books = []
+        for book_id in sorted_books:
+            if book_id in self.scanned_books:
+                continue
+            else:
+                chosen_books.append(book_id)
+                self.scanned_books.add(book_id)
+                added_books += 1
+                if added_books >= days_to_scan:
+                    break
+        if len(chosen_books) < days_to_scan:
+            print (f"WARNING: Library {library.index} has {days_to_scan} days but scanned {len(chosen_books)} books")
+        self.chosen_books[library.index] = chosen_books
