d58f25bdd26cd655a4f89b3795643f5011179578
shaul.rosner shaul.rosner <shaul.rosner@gmail.com>
2020-02-20 20:13:30 +0200

    working on add book

--- book_choice.py
+++ book_choice.py
@@ -3,5 +3,10 @@ class BookChooser:
         self.scores = scores
         self.days = days
         self.scanned_books = set()
+        self.chosen_books = {}
 
-    def add_library(self, library, day):
+    def add_library(self, library, days_to_scan):
+        sorted_books = sorted(list(library.book_ids), key=lambda book_id:self.scores[book_id], reverse=True)
+        added_books = 0
+        for book in sorted_books:
+            
