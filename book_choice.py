cf88cf6dda0d98bc9dac98247ced9c079e32debf
shaul.rosner shaul.rosner <shaul.rosner@gmail.com>
2020-02-20 20:09:08 +0200

    added constructor

--- book_choice.py
+++ book_choice.py
@@ -1,2 +1,7 @@
 class BookChooser:
-    def __init__(self):
+    def __init__(self, days, scores):
+        self.scores = scores
+        self.days = days
+        self.scanned_books = set()
+
+    def add_library(self, library, day):
