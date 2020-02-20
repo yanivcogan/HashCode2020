508257e052e16834bd06a3655b2ef7573c2dd060
shaul.rosner shaul.rosner <shaul.rosner@gmail.com>
2020-02-20 20:23:21 +0200

    added library scans

--- book_choice.py
+++ book_choice.py
@@ -1,17 +1,19 @@
-from classes import LibraryScan
+from typing import List
+
+from classes import LibraryScan, Library
 
 
 class BookChooser:
-    def __init__(self, days, scores):
-        self.scores = scores
-        self.days = days
-        self.scanned_books = set()
-        self.library_scans = []
+    def __init__(self, days:int, scores:List[int]):
+        self.scores:List[int] = scores
+        self.days:int = days
+        self.scanned_books:set = set()
+        self.library_scans:List[LibraryScan] = []
 
-    def add_library(self, library, days_to_scan):
-        sorted_books = sorted(library.book_ids, key=lambda book_id:self.scores[book_id], reverse=True)
-        added_books = 0
-        chosen_books = []
+    def add_library(self, library:Library, days_to_scan:int) -> None:
+        sorted_books:List[int] = sorted(library.book_ids, key=lambda book_id:self.scores[book_id], reverse=True)
+        added_books:int = 0
+        chosen_books:List[int] = []
         for book_id in sorted_books:
             if book_id in self.scanned_books:
                 continue
@@ -25,5 +27,5 @@ class BookChooser:
             print (f"WARNING: Library {library.index} has {days_to_scan} days but scanned {len(chosen_books)} books")
         self.library_scans.append(LibraryScan(library, chosen_books))
 
-    def get_scans(self):
+    def get_scans(self)->List[LibraryScan]:
         return self.library_scans
