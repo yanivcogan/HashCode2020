ef67f9f85bb3a53b323ef99b7dace1e0b49ae6b3
yanivcogan <yanivcogan89@gmail.com>
2020-02-20 20:50:11 +0200

    add library picker

---
+++ library_picker.py
@@ -0,0 +1,22 @@
+from typing import List, Set
+from classes import Library
+
+
+def pick_library(libraries: List[Library], book_scores: List[int], days_left: int, hist: Set[int] = None) -> Library:
+    if hist is None:
+        hist = {}
+    max_intersect_score = 0
+    max_intersect_index = 0
+    for i in range(len(libraries)):
+        library = libraries[i]
+        if library.signup_time >= days_left:
+            continue
+        score: int = score_set(book_scores, library.book_ids & hist)
+        if score > max_intersect_score:
+            max_intersect_index = i
+            max_intersect_score = score
+    return libraries[max_intersect_index]
+
+
+def score_set(book_scores: List[int], book_ids: Set[int]) -> int:
+    return sum(book_scores[i] for i in book_ids)
